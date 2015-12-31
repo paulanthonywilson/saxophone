6c20e2259a1b28c7477b613f074bebf107fe2f56
Paul Wilson <paulanthonywilson@gmail.com>
2015-12-31 17:40:10 +0000

    a bit of html

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 
-config :saxophone, :cowboy_options, [port: 8080]
+config :saxophone, :cowboy_options, [port: 4000]
 config :saxophone, :start_ethernet, false
 
 import_config "#{Mix.env}.exs"
--- lib/saxophone/router.ex
+++ lib/saxophone/router.ex
@@ -9,11 +9,49 @@ defmodule Saxophone.Router do
     {:ok, _} = Plug.Adapters.Cowboy.http __MODULE__, [], cowboy_options
   end
 
+  post "/light_on" do
+    digital_write(17, 1)
+    send_resp(conn, 200, "The light is on!" |> web_page)
+  end
+
+  post "/light_off" do
+    digital_write(17, 0)
+    send_resp(conn, 200, "The light is off!" |> web_page)
+  end
+
   get "/" do
-    send_resp(conn, 200, "Hello, I am working!")
+    send_resp(conn, 200, "Hello" |> web_page)
   end
 
   match _ do
     send_resp(conn, 404, "Not found")
+  end
+
+  defp digital_write(pin, value) do
+    {:ok, pid} = Gpio.start_link(pin, :output)
+    Gpio.write(pid, value)
+    IO.puts "Wrote #{value} to pin #{pin}"
+    Process.exit(pid, :normal)
+    value
+  end
+
+
+  defp web_page(message) do
+    """
+    <html>
+      <head>
+        <title>Led thingy</title>
+      </head>
+      <body>
+        <p>#{message}</p>
+        <form action = "/light_on" method="post">
+          <input type="submit" value="Turn light on"></input>
+        </form>
+        <form action = "/light_off" method="post">
+          <input type="submit" value="Turn light off"></input>
+        </form>
+      </body>
+    </html>
+    """
   end
 end
