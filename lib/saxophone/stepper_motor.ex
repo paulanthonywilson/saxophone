7edc36152b788af8a7208fdad6c0c872d065c8ff
Paul Wilson <paulanthonywilson@gmail.com>
2016-04-19 13:38:47 +0100

    stepper with tests

--- lib/saxophone/stepper_motor.ex
+++ lib/saxophone/stepper_motor.ex
@@ -1,5 +1,8 @@
 defmodule Saxophone.StepperMotor do
   use GenServer
+  alias Saxophone.StepperMotor
+
+  defstruct pins: [], direction: :neutral, position: 0, step_millis: 100, timer_ref: nil
 
   @position_pin_values [
     [0, 0, 0, 1],
@@ -31,10 +34,14 @@ defmodule Saxophone.StepperMotor do
   end
 
   @doc """
-  Set the step rate in millisections
+  Set the step rate in milliseconds
   """
   def set_step_rate(pid, step_rate) do
     pid |> GenServer.call({:set_step_rate, step_rate})
+  end
+
+  def state(pid) do
+    pid |> GenServer.call(:get_state)
   end
 
   ## Callbacks
@@ -45,38 +52,51 @@ defmodule Saxophone.StepperMotor do
     end)
 
     send(self, :step)
-    {:ok, %{pins: gpio_pins, direction: :neutral, step_millis: 100, position: 0}}
+    {:ok, %StepperMotor{pins: gpio_pins}}
   end
 
   def handle_call({:set_direction, direction}, _from, status) do
-    send(self, :step)
-    {:reply, :ok, %{status | direction: direction}}
+    timer_ref = schedule_next_step(direction, status.step_millis, status.timer_ref)
+    {:reply, :ok, %{status | direction: direction, timer_ref: timer_ref}}
   end
 
   def handle_call({:set_step_rate, step_rate}, _from, status) do
-    send(self, :step)
-    {:reply, :ok, %{status | step_millis: step_rate}}
+    timer_ref = schedule_next_step(status.direction, step_rate, status.timer_ref)
+    {:reply, :ok, %{status | step_millis: step_rate, timer_ref: timer_ref}}
   end
 
-  def handle_info(:step, status = %{pins: pins, direction: direction, step_millis: step_millis, position: position}) do
+  def handle_call(:get_state, _from, status) do
+    {:reply, status, status}
+  end
+
+  def handle_info(:step, status = %{pins: pins,
+                                    direction: direction,
+                                    step_millis: step_millis,
+                                    position: position}) do
+    new_position = new_position(position, direction)
     @position_pin_values
-    |> Enum.at(position)
+    |> Enum.at(new_position)
     |> Enum.zip(pins)
     |> Enum.each(fn {value, pin} ->
       pin |> Gpio.write(value)
     end)
 
-    step_again(direction, step_millis)
+    timer_ref = schedule_next_step(direction, step_millis)
 
-    {:noreply, %{status | position: new_position(position, direction)}}
+    {:noreply, %{status | position: new_position, timer_ref: timer_ref}}
   end
 
-  defp step_again(:neutral, _) do
+  defp schedule_next_step(direction, step_millis, timer_ref) do
+    cancel_timer(timer_ref)
+    schedule_next_step(direction, step_millis)
   end
 
-  defp step_again(_, step_millis) do
-    Process.send_after(self, :step, step_millis)
-  end
+  defp schedule_next_step(:neutral, _step_millis), do: nil
+  defp schedule_next_step(_direction, step_millis), do: Process.send_after(self, :step, step_millis)
+
+
+  defp cancel_timer(:nil), do: false
+  defp cancel_timer(timer_ref), do: Process.cancel_timer(timer_ref)
 
   defp new_position(position, :neutral) do
     position
