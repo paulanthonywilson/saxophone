92587d6379735389da9f99a79b9e603028a3aea5
Paul Wilson <paulanthonywilson@gmail.com>
2016-05-04 16:34:07 +0100

    sax bot silliness

--- lib/saxophone/slack_bot.ex
+++ lib/saxophone/slack_bot.ex
@@ -6,6 +6,12 @@ defmodule Saxophone.SlackBot do
 
   def play_sax(_pid, message) do
     IO.inspect {:play, message}
+    say self, message["channel"], "Oh, bugger off"
     Saxophone.Saxophonist.play(:saxophonist)
+  end
+
+  def handle_cast({:handle_incoming, event, message}, state) do
+    IO.inspect {:incoming, event, message}
+    {:noreply, state}
   end
 end
